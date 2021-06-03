11ff70a341e7f25b5f5c644941da82bdd156be22
lexbros <aalvl@mail.ru>
2021-06-03 15:35:34 +0300

    learn_OOP

--- 002_vid_selfedu_task_1.py
+++ 002_vid_selfedu_task_1.py
@@ -4,7 +4,9 @@
 # получать их (в виде кортежа).
 
 class Point3D:
-    coords = []
+    coords = []  # список хранит ссылки на __dict__ каждого экземпляра класса
+    # таким образом, изменяя атрибуты экземпляра, они автоматически изменяются в
+    # списке всех экземпляров
 
     def __init__(self, x, y, z):
         self.x = x
@@ -22,11 +24,14 @@ class Point3D:
 
 
 p_1 = Point3D(0, 0, 0)
-print(Point3D.coords)
+print(id(Point3D.coords[0]))
+# print(Point3D.coords)
 p_1.set_coords(1, 2, 3)
-print(Point3D.coords)
-print(p_1.get_coords())
+print(id(Point3D.coords[0]))
+# print(Point3D.coords)
+# print(p_1.get_coords())
 p_2 = Point3D(8, 8, 8)
-print(Point3D.coords)
+# print(Point3D.coords)
 p_1.set_coords(9, 9, 9)
-print(Point3D.coords)
+# print(Point3D.coords)
+print(id(Point3D.coords[0]))
