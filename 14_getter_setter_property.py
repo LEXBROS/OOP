07c5857e67162976691679b196b542611eacbfb4
lexbros <aalvl@mail.ru>
2021-06-06 13:38:46 +0300

    learn_OOP

---
+++ 14_getter_setter_property.py
@@ -0,0 +1,24 @@
+class UserMail:
+
+    def __init__(self, login, email):
+        self.login = login
+        self.__email = email
+
+    def get_email(self):
+        return self.__email
+
+    def set_email(self, email: str):
+        if email.count('@') == 1 and '.' in email[email.index('@'):]:
+            self.__email = email
+        else:
+            print('Ошибочная почта')
+
+    email = property(get_email, set_email)
+
+
+k = UserMail('belosnezhka', 'prince@wait.you')
+print(k.email)  # prince@wait.you
+k.email = [1, 2, 3]  # Ошибочная почта
+k.email = 'prince@still@.wait'  # Ошибочная почта
+k.email = 'prince@still.wait'
+print(k.email)  # prince@still.wait
